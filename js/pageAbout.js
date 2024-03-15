0231ec013f43422d8355b3d7025dd9a89815cfb6
folivier <fabien.olivier2@gmail.com>
2024-03-15 03:42:26 +0100

    page annexe

--- js/pageAbout.js
+++ js/pageAbout.js
@@ -8,13 +8,33 @@ export function pageAbout() {
     }
     container.classList.add('about');
 
-    let subcontainerEllye = document.createElement('div');
+    let subcontainerAbout = document.createElement('div');
+    subcontainerAbout.className = 'subcontainerAbout';
 
-    let linkEllye = document.createElement('a');
-    linkEllye.href = 'https://www.ellye.fr';
-    linkEllye.target = 'blank';
-    linkEllye.textContent = "Site de l'association Ellye";
-    subcontainerEllye.appendChild(linkEllye);
+    let descriptif = document.createElement('div');
+    descriptif.textContent = `Application imaginé et réalisé lors d'un challenge inter-école du campus Saint-Marc avec les écoles PIGIERS, ISCOM et Zone 01`;
+    subcontainerAbout.appendChild(descriptif)
+
+    let concept = document.createElement('h3');
+    concept.textContent = 'Concept crée par :';
+    subcontainerAbout.appendChild(concept);
+    let contentConcept = document.createElement('div');
+    contentConcept.innerHTML = `
+    • BAILLEUL Lilou</br>
+    • COUSIN Lilou</br>
+    • HORVILLE Anthony</br>
+    • MUSTEL Quentin</br>
+    • SAINT-AUBUN Gabrielle</br>
+    • TEMPEZ Zoé<br>
+    • THOREL Marie<br>
+    `;
+    subcontainerAbout.appendChild(contentConcept);
+    let creation = document.createElement('h3');
+    creation.textContent = 'Application développé par :';
+    subcontainerAbout.appendChild(creation);
+    let contentcreation = document.createElement('div');
+    contentcreation.innerHTML = '• OLIVIER Fabien';
+    subcontainerAbout.appendChild(contentcreation);
     
-    container.appendChild(subcontainerEllye);
+    container.appendChild(subcontainerAbout);
 }
--- js/pageEllye.js
+++ js/pageEllye.js
@@ -8,4 +8,32 @@ export function pageEllye() {
     }
     container.classList.add('ellye');
     
+    let subcontainerEllye = document.createElement('div');
+    subcontainerEllye.className = 'subcontainerEllye';
+
+    let divimg = document.createElement('div');
+    divimg.className = 'divimg';
+    let logoEllye = document.createElement('img');
+    logoEllye.className = 'logoEllye';
+    logoEllye.src = './assets/img/logo_ellye.png';
+    divimg.appendChild(logoEllye)
+    subcontainerEllye.appendChild(divimg);
+
+    let nameAsso = document.createElement('h3');
+    nameAsso.textContent = `ELLyE – Ensemble Leucémie Lymphomes Espoir est une association de patients et de proches de loi 1901 spécialisée dans les lymphomes, la leucémie lymphoïde chronique et la maladie de Waldenström.`;
+    subcontainerEllye.appendChild(nameAsso);
+
+    let description = document.createElement('div');
+    description.innerHTML = `
+    Créée en 2006 sous le nom de France Lymphome Espoir, elle obtient son agrément national du ministère de la santé en 2018, lui permettant de représenter officiellement les malades dans les instances réglementées.</br>
+    En juin 2021 les associations France Lymphome Espoir et SILLC décident de fusionner pour créer une seule entité, ELLyE.`;
+    subcontainerEllye.appendChild(description);
+
+    let linkEllye = document.createElement('a');
+    linkEllye.href = 'https://www.ellye.fr';
+    linkEllye.target = 'blank';
+    linkEllye.textContent = "Site de l'association Ellye";
+    subcontainerEllye.appendChild(linkEllye);
+    
+    container.appendChild(subcontainerEllye);
 }
